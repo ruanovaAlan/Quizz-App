8035b6a69cca8bc0bb5fd12de5bd55d6feea0e36
Alan Ruanova <ruanovaalan96@gmail.com>
2024-02-06 20:41:40 -0600

    Add Quiz component and questions.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Header from './components/Header.jsx'
+import Quiz from './components/Quiz.jsx'
 
 function App() {
     return (
-        <Header />
+        <>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </>
     )
 }
 
