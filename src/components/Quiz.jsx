a153efdc2cdc48ad7b53e538efd218a56cb9b243
Alan Ruanova <ruanovaalan96@gmail.com>
2024-02-13 22:05:03 -0600

    Add quiz completion message and shuffle answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-
+import quizComleteImg from '../assets/quiz-complete.png'
 import QUESTIONS from '../questions.js';
 
 export default function Quiz() {
     const [userAnswers, setUsersAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUsersAnswers((prevUserAnswers) => {
@@ -14,12 +15,22 @@ export default function Quiz() {
         console.log(userAnswers)
     }
 
+    if (quizIsComplete) {
+        return <div id='summary'>
+            <img src={quizComleteImg} alt="Trophy Icon" />
+            <h2>Quiz is completed</h2>
+        </div>
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id='quiz'>
             <div id='question'>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id='answers'>
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+                    {shuffledAnswers.map((answer) => {
                         return (
                             <li className='answer' key={answer}>
                                 <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
